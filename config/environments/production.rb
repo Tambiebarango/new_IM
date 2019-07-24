fd5d3c1a597caee49fced2398b6c8948d350f6c2
Theodore Barango <tambi088@uottawa.ca>
2019-07-24 19:59:26 -0400

    redis config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,8 +43,8 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  #config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.url = 'wss://theoinstantmessaging.herokuapp.com/cable'
+  config.action_cable.allowed_request_origins = [ 'http://theoinstantmessaging.herokuapp.com', 'https://theoinstantmessaging.herokuapp.com' ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
